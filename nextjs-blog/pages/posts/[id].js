8ca6de0b98041194bc0c9082fcfcb7beeef9d057
Oerjan Hoel <orjan.hoel@gmail.com>
2025-09-21 09:11:20 -0700

    Week 5 Initial push

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,48 @@
+import Layout from '../../components/layout'; // Imports the layout from compnents folder
+import { getAllPostIds, getPostData } from '../../lib/posts'; // Imports the code we ned to structure out page
+import Head from 'next/head'; // Imports head from Next.js
+import Date from '../../components/date'; // Imports Date from the date module we installed
+import utilStyles from '../../styles/utils.module.css'; // Imports the CSS from utils.module.css file 
+
+
+// Exports the layout of the post data and adds CSS
+// Structures the data in to JSX
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+// Gets teh staic paths for the blog posts 
+export async function getStaticPaths() {
+  // Variable for the Post id's
+  const paths = getAllPostIds();
+  return {
+    // If the path is not found it won't fall back
+    paths,
+    fallback: false,
+  };
+}
+
+// Exports the Static Props from 
+export async function getStaticProps({ params }) {
+  // Uses async to post the props and data to our blog posts
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
